ed5fda12e3fce84f2388f9d87182ac73a1b030ca
Ashraful Haque Akash <31436514+ashrafulhaqueakash@users.noreply.github.com>
2019-02-11 16:22:15 +0600

    Add files via upload

---
+++ welcome.area.js
@@ -0,0 +1,62 @@
+    // *********************************
+    // :: 4.0 Welcome Slides Active Code
+    // *********************************
+
+    if ($.fn.owlCarousel) {
+        var welcomeSlider = $('.welcome-slides');
+        welcomeSlider.owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            smartSpeed: 1000,
+            autoplayTimeout: 10000,
+            nav: true,
+            animateIn: 'fadeInDown',
+            animateOut: 'fadeOut'
+        })
+
+        welcomeSlider.on('translate.owl.carousel', function () {
+            var layer = $("[data-animation]");
+            layer.each(function () {
+                var anim_name = $(this).data('animation');
+                $(this).removeClass('animated ' + anim_name).css('opacity', '0');
+            });
+        });
+
+        $("[data-delay]").each(function () {
+            var anim_del = $(this).data('delay');
+            $(this).css('animation-delay', anim_del);
+        });
+
+        $("[data-duration]").each(function () {
+            var anim_dur = $(this).data('duration');
+            $(this).css('animation-duration', anim_dur);
+        });
+
+        welcomeSlider.on('translated.owl.carousel', function () {
+            var layer = welcomeSlider.find('.owl-item.active').find("[data-animation]");
+            layer.each(function () {
+                var anim_name = $(this).data('animation');
+                $(this).addClass('animated ' + anim_name).css('opacity', '1');
+            });
+        });
+
+        function welcomeSlide() {
+            $('.owl-item').removeClass('prev next');
+            var currentSlide = $('.welcome-slides .owl-item.active');
+            currentSlide.next('.owl-item').addClass('next');
+            currentSlide.prev('.owl-item').addClass('prev');
+            var nextSlideImg = $('.owl-item.next').find('.single-welcome-slide').attr('data-img-url');
+            var prevSlideImg = $('.owl-item.prev').find('.single-welcome-slide').attr('data-img-url');
+            $('.owl-nav .owl-prev').css({
+                background: 'url(' + prevSlideImg + ')'
+            });
+            $('.owl-nav .owl-next').css({
+                background: 'url(' + nextSlideImg + ')'
+            });
+        }
+
+        welcomeSlider.on('translated.owl.carousel', function () {
+            welcomeSlide();
+        });
+    }
